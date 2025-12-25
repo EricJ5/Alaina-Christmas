386f99bdb011458971330063ecb1416a25d91c2c
Eric Johnson <erijohnson0701@gmail.com>
2025-12-25 09:02:23 -0500

    set everything up fixed some possible crashes

--- mpv.c
+++ mpv.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdatomic.h>
 #include <syscall.h>
 #include <unistd.h>
@@ -10,20 +11,31 @@
 #include <stdlib.h>
 #include <mpv/client.h>
 #include <sys/types.h>
-#include <wiringPi.h>
+#include <spawn.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+//#include <wiringPi.h>
+#include <signal.h>
+
 
 atomic_flag downloading = ATOMIC_FLAG_INIT;
 volatile bool updated = false;
 char *playlist = "./Songs/playlist.m3u";
 int mode = 1;
+extern char **environ;
 #define VOLUME_UP 3
 #define VOLUME_DOWN 2
 #define SKIP 0
 #define PREV 8
 #define MODE 9
 #define RESET 7
+static volatile sig_atomic_t running = 1;
 
-
+static void handle_sigterm(int sig)
+{
+    (void)sig;
+    running = 0;
+}
 void gpioinit() {
 	wiringPiSetup();
 	
@@ -131,17 +143,41 @@ int read_mcp3202_channel(uint8_t channel) {
 
 void *UpdatePlaylist(void *arg) {
 	while (atomic_flag_test_and_set(&downloading)) {
-	}
-	system("rm ./Songs/playlist.m3u");
+	};
+	unlink(playlist)
+	pid_t pid1;
+	pid_t pid2;
+	posix_spawn_file_actions_t fa;
+    	posix_spawn_file_actions_init(&fa);
+	int fd = open(playlist,
+                  O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	posix_spawn_file_actions_adddup2(&fa, fd, STDOUT_FILENO);
+    	posix_spawn_file_actions_addclose(&fa, fd);
 	char *Songs = "./Songs";
 	char *archive = "./Songs/archive.txt";
-	char ytdlpcommand[100];
-	char ytdlplistcommand[100];
+	char ytdlpcommand[150];
+	char ytdlplistcommand[150];
 	printf("%s \n", yturl);
-	snprintf(ytdlpcommand, 150, "yt-dlp -x --audio-format mp3 --download-archive %s %s", archive, yturl);
-	snprintf(ytdlplistcommand, 150, "yt-dlp --flat-playlist --print \"%(title)s.mp3\" %s > %s", yturl, playlist);
-	system(ytdlpcommand);
-	system(ytdlplistcommand);
+   	char *argv2[] = {
+        	"yt-dlp",
+        	"--flat-playlist",
+        	"--print", "\"%(title).mp3\"",
+        	(char *)yturl,
+        	NULL
+    	};
+	char *argv1[] = {
+        	"yt-dlp",
+        	"-x",
+        	"--audio-format", "mp3",
+        	"--download-archive", (char *)archive,
+        	(char *)yturl,
+       	 	NULL
+   	};
+	posix_spawnp(&pid1, "yt-dlp", NULL, NULL, argv1, environ);
+	posix_spawnp(&pid2, "yt-dlp", &fa, NULL, argv2, environ);
+	posix_spawn_file_actions_destroy(&fa);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
     	atomic_flag_clear(&downloading);
 	updated = true;
 	return NULL;	
@@ -156,7 +192,7 @@ void *ButtonLoop(void *handle) {
 	const char *voldowncommand[] ={"add", "volume", "-5", NULL};
 	pthread_t pid;
 	gpioinit();
-	for (;;) {
+	while (running) {
 		if (!updated) {
 			if (digitalRead(VOLUME_UP) ==  LOW) {
 				printf("volume up\n");
@@ -197,34 +233,41 @@ void *ButtonLoop(void *handle) {
 			updated = false;
 		}
 	}
+	return NULL
 }
 
 
 void *MPVEventLoop(void *handle) {
-    mpv_handle *mpv = (mpv_handle*)handle;
-    while (1) {
-        mpv_event *ev = mpv_wait_event(mpv, 0.1);
-        if (ev->event_id == MPV_EVENT_SHUTDOWN)
-            break;
-    }
+    	mpv_handle *mpv = (mpv_handle*)handle;
+    	while (running) {
+       		mpv_event *ev = mpv_wait_event(mpv, 0.1);
+        	if (ev->event_id == MPV_EVENT_SHUTDOWN)
+            	break;
+    	}
+	return NULL
 }
 
 
 int main(int argc,char *argv[]) {
 
+    	struct sigaction sa = {0};
+    	sa.sa_handler = handle_sigterm;
+    	sigemptyset(&sa.sa_mask);
+    	sa.sa_flags = 0;
+
+    	sigaction(SIGTERM, &sa, NULL);
+    	sigaction(SIGINT,  &sa, NULL);
 	yturl = argv[1];
 	static double speedarray[3995];
 
 	double increment = (double) 3/3996;
 
-	printf("%f\n", increment);
 	double count = .35;
 	for (int i = 0;i < 3096; i++) {
 		speedarray[i] = count;
 		count += increment;
 	}
 	printf("%s", argv[1]);
-	printf("hey-1\n");
 	mpv_handle *mpv = mpv_create();
 	
 	mpv_set_option_string(mpv, "vid", "no");
@@ -234,20 +277,17 @@ int main(int argc,char *argv[]) {
 	mpv_initialize(mpv);
 	pthread_t mpv_thread;
 	pthread_create(&mpv_thread, NULL, MPVEventLoop, mpv);
-   	printf("hey \n", NULL);
 	pthread_t id;	
 	pthread_create(&id, NULL, ButtonLoop, mpv);
-	printf("hey before initial playlist \n");
-	system("mkdir Songs");
+	mode_t perms = S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH;
+	mkdir("./Songs", perms);
 	UpdatePlaylist(NULL);
-	printf("hey after initial playlist \n");
 
 
-	printf("hey2\n");
 	char speed[15];
 	spi_init();
 	double normalspeed = 1.00f;
-	for (;;) {
+	while (running) {
 		int adc_value = read_mcp3202_channel(0);
 		if (adc_value > 30) {
 			unstop(mpv);
@@ -255,8 +295,9 @@ int main(int argc,char *argv[]) {
 			printf("%f\n", speedarray[adc_value-100]);
 			if (mode == 1) {
 				set_speed(mpv, speedarray[adc_value-100], speed);
+			} else {
+				set_speed(mpv, normalspeed, speed);
 			}
-			set_speed(mpv, normalspeed, speed);			
 		} else  {
 			if (mode==3) {
 				unstop(mpv);
